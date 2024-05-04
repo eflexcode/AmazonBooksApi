fac186e85ba33884344bbf55d50617ab702046fa
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-05-04 03:12:47 +0100

    working on util class

--- src/main/java/com/ifeanyi/AmazonBooksApi/util/Util.java
+++ src/main/java/com/ifeanyi/AmazonBooksApi/util/Util.java
@@ -1,7 +1,9 @@
 package com.ifeanyi.AmazonBooksApi.util;
 
+import com.ifeanyi.AmazonBooksApi.book.entity.Books;
 import com.opencsv.CSVReader;
 
+import java.awt.print.Book;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -20,13 +22,18 @@ public class Util {
         try {
             CSVReader csvReader = new CSVReader(new FileReader(csvDirectory), ';');
             for (int i = 0 ; i < 120 ;i ++){
-                p.add( csvReader.readNext());
-//                for (int j = 0 ; j < 120 ;j ++){
-                    System.out.println(Arrays.toString(p.get(i)));
-//                }
+                p.add(csvReader.readNext());
+
+                for (int h = 0; h<p.get(i).length ; h++){
+                    System.out.println(p.get(i)[h]);
+                    Books book = new Books();
+                    if (h == 0){
+                        book.setISBN(p.get(i)[h]);
+                    }
+                }
+
+
             }
-
-
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
