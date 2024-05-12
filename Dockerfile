2efc45710e3853b41f107d5b72908238a90a9c6f
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-05-12 13:53:05 +0100

    kinda done

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:21-jdk-alpine
+ARG JAR_FILE=*.jar
+COPY target/AmazonBooksApi-0.0.1.jar AmazonBooksApi-0.0.1.jar
+ENTRYPOINT ["java", "-jar", "AmazonBooksApi-0.0.1.jar"]
--- src/main/java/com/ifeanyi/AmazonBooksApi/AmazonBooksApiApplication.java
+++ src/main/java/com/ifeanyi/AmazonBooksApi/AmazonBooksApiApplication.java
@@ -27,57 +27,57 @@ public class AmazonBooksApiApplication {
 //
     }
 
-//    @Bean
-//    public CommandLineRunner commandLineRunner(BooksService booksService){
-//
-//        return args -> {
-//
-//            List<String[]> p = new ArrayList<>();
-//            List<Books> books = new ArrayList<>();
-//
-//            try {
-//                CSVReader csvReader = new CSVReader(new FileReader("C:\\Users\\E.F.Lhomes\\Downloads\\archive(4)\\books2.csv"), ';');
-//                for (int i = 0; i < 271379; i++) {
-//
-//                    Books book = new Books();
-//                    p.add(csvReader.readNext());
-//
-//                    for (int h = 0; h <8; h++) {
-//
-//
-//                        if (h == 0) {
-//                            book.setIsbn(p.get(i)[h] != null  ?  p.get(i)[h] : null);
-//                        } else if (h == 1) {
-//                            book.setTitle(p.get(i)[h] != null  ?  p.get(i)[h] : null);
-//                        } else if (h == 2) {
-//                            book.setAuthor(p.get(i)[h] != null  ?  p.get(i)[h] : null);
-//                        } else if (h == 3) {
-//                            book.setYearOfPublication(Integer.valueOf(p.get(i)[h] != null  ?  p.get(i)[h] : "0"));
-//                        } else if (h == 4) {
-//                            book.setPublisher(p.get(i)[h] != null  ?  p.get(i)[h] : null);
-//                        } else if (h == 5) {
-//                            book.setImageUrlS(p.get(i)[h] != null  ?  p.get(i)[h] : null);
-//                        } else if (h == 6) {
-//                            book.setImageUrlM(p.get(i)[h] != null  ?  p.get(i)[h] : null);
-//                        } else {
-//                            book.setImageUrlL(p.get(i)[h] != null  ?  p.get(i)[h] : null);
-//                        }
-//
-//                    }
-//
-////                    System.out.println(book);
-////                    booksService.createBook(book).subscribe();
-//                    books.add(book);
-//                    if (i == 271378){
-//                        booksService.saveMany(books).subscribe();
-////                        System.out.println(books);
-//                    }
-//
-//                }
-//
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//        };
-//    }
+    @Bean
+    public CommandLineRunner commandLineRunner(BooksService booksService){
+
+        return args -> {
+
+            List<String[]> p = new ArrayList<>();
+            List<Books> books = new ArrayList<>();
+
+            try {
+                CSVReader csvReader = new CSVReader(new FileReader("C:\\Users\\E.F.Lhomes\\Downloads\\archive(4)\\books2.csv"), ';');
+                for (int i = 0; i < 271379; i++) {
+
+                    Books book = new Books();
+                    p.add(csvReader.readNext());
+
+                    for (int h = 0; h <8; h++) {
+
+
+                        if (h == 0) {
+                            book.setIsbn(p.get(i)[h] != null  ?  p.get(i)[h] : null);
+                        } else if (h == 1) {
+                            book.setTitle(p.get(i)[h] != null  ?  p.get(i)[h] : null);
+                        } else if (h == 2) {
+                            book.setAuthor(p.get(i)[h] != null  ?  p.get(i)[h] : null);
+                        } else if (h == 3) {
+                            book.setYearOfPublication(Integer.valueOf(p.get(i)[h] != null  ?  p.get(i)[h] : "0"));
+                        } else if (h == 4) {
+                            book.setPublisher(p.get(i)[h] != null  ?  p.get(i)[h] : null);
+                        } else if (h == 5) {
+                            book.setImageUrlS(p.get(i)[h] != null  ?  p.get(i)[h] : null);
+                        } else if (h == 6) {
+                            book.setImageUrlM(p.get(i)[h] != null  ?  p.get(i)[h] : null);
+                        } else {
+                            book.setImageUrlL(p.get(i)[h] != null  ?  p.get(i)[h] : null);
+                        }
+
+                    }
+
+//                    System.out.println(book);
+//                    booksService.createBook(book).subscribe();
+                    books.add(book);
+                    if (i == 271378){
+                        booksService.saveMany(books).subscribe();
+//                        System.out.println(books);
+                    }
+
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
 }
